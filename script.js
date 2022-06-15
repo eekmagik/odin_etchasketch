73cfdab67f0f5901f0dc70e90defe8064e9600aa
eekmagik <dx3906@protonmail.com>
2022-06-14 21:37:02 -0700

    Clear grid element on button click

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ function mouseOut(cell) {
 }
 
 function gridSize() {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
   let size = prompt("Size: 1-100");
   if (size > 100 || size < 1 || size === NaN) {
     document.getElementsByClassName('header').textContent = "Choice out of range"
@@ -17,7 +20,7 @@ function gridSize() {
     for (let i=0; i<size**2; i++) {
       const cell = document.createElement("div");
       cell.style.aspectRatio = "1 / 1";
-      //cell.style.border = "solid black 1px";
+      cell.style.border = "solid black 1px";
       cell.onmouseover = function() {mouseOver(cell)};
       //cell.onmouseout = function() {mouseOut(cell)};
       grid.appendChild(cell);
