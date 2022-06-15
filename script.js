aec9ecb4d1bc46d8fde5776ada1bbc8ab330c163
eekmagik <dx3906@protonmail.com>
2022-06-14 18:50:22 -0700

    Set aspect ratio of cells

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const grid = document.getElementById('container');
 
-
 for (let i=0; i<16; i++) {
   const cell = document.createElement("div");
+  cell.style.aspectRatio = "1 / 1";
+  cell.style.border = "solid black 1px";
   grid.appendChild(cell);
 }
+  
