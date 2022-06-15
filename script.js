dd9473f99bca167cf8deed119d6f85db41cf50f7
eekmagik <dx3906@protonmail.com>
2022-06-14 21:28:00 -0700

    Remove redundant function

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 const grid = document.getElementById('container');
 
-for (let i=0; i<16; i++) {
-  const cell = document.createElement("div");
-  cell.style.aspectRatio = "1 / 1";
-  cell.style.border = "solid black 1px";
-  cell.onmouseover = function() {mouseOver(cell)};
-  //cell.onmouseout = function() {mouseOut(cell)};
-  grid.appendChild(cell);
-}
-  
 function mouseOver(cell) {
   cell.style.backgroundColor = "black";
 }
@@ -16,3 +7,21 @@ function mouseOver(cell) {
 function mouseOut(cell) {
   cell.style.backgroundColor = "white";
 }
+
+function gridSize() {
+  let size = prompt("Size: 1-100");
+  if (size > 100 || size < 1 || size === NaN) {
+    document.getElementsByClassName('header').textContent = "Choice out of range"
+  }else {
+    grid.style.gridTemplateColumns = "repeat(" + size + ", 1fr)";
+    for (let i=0; i<size**2; i++) {
+      const cell = document.createElement("div");
+      cell.style.aspectRatio = "1 / 1";
+      //cell.style.border = "solid black 1px";
+      cell.onmouseover = function() {mouseOver(cell)};
+      //cell.onmouseout = function() {mouseOut(cell)};
+      grid.appendChild(cell);
+    }
+  }
+}
+
