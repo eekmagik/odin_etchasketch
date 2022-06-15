5da5f68c9d6cbd088b6cff5570ed8335701587e7
eekmagik <dx3906@protonmail.com>
2022-06-14 19:08:42 -0700

    Change cell color on hover

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ for (let i=0; i<16; i++) {
   const cell = document.createElement("div");
   cell.style.aspectRatio = "1 / 1";
   cell.style.border = "solid black 1px";
+  cell.onmouseover = function() {mouseOver(cell)};
+  //cell.onmouseout = function() {mouseOut(cell)};
   grid.appendChild(cell);
 }
   
+function mouseOver(cell) {
+  cell.style.backgroundColor = "black";
+}
+
+function mouseOut(cell) {
+  cell.style.backgroundColor = "white";
+}
